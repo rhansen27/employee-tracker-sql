f3b869a3b81be1525e099d94ef31cdc8bd8a8fc9
rph <rhansen0127@gmail.com>
2024-08-05 11:04:46 -0500

    small refactor and added inserting of employee data

--- utils/queries.js
+++ utils/queries.js
@@ -23,15 +23,36 @@ const getDepartments = async () => {
 };
 
 const getEmployees = async () => {
-  const employees = await pool.query(`
-        SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, departments.department, roles.salary,
-        CASE 
-            WHEN employees.managers.first_name IS NOT NULL THEN concat_ws(' ', managers.first_name, managers.last_name
-            ELSE NULL
-        END AS manager
-        FROM employees
-        JOIN roles ON employees.role_id = roles.id
-        JOIN departments ON roles.department_id = departments.id
-        LEFT JOIN employees AS managers ON employees.manager_id = managers.id`);
-  return employees.rows;
+  try {
+    const employees = await pool.query(`
+            SELECT employees.id, employees.first_name, employees.last_name, roles.job_title, departments.department, roles.salary,
+            CASE 
+                WHEN employees.managers.first_name IS NOT NULL THEN concat_ws(' ', managers.first_name, managers.last_name
+                ELSE NULL
+            END AS manager
+            FROM employees
+            JOIN roles ON employees.role_id = roles.id
+            JOIN departments ON roles.department_id = departments.id
+            LEFT JOIN employees AS managers ON employees.manager_id = managers.id`);
+    return employees.rows;
+  } catch (err) {
+    throw "Error retrieving employees";
+  }
+};
+
+const insertEmployeeData = async (
+  first_name,
+  last_name,
+  role_id,
+  manager_id
+) => {
+  try {
+    const employeeData = await pool.query(
+      `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+        VALUES ($1, $2, $3, $4);`,
+      [first_name, last_name, role_id, manager_id]
+    );
+  } catch (err) {
+    throw "Error inserting employee data into database";
+  }
 };
