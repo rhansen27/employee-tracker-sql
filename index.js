e0cf9cbd85d149bbf3127f98b76d24c916f78bec
rph <rhansen0127@gmail.com>
2024-08-20 14:21:03 -0500

    added inquirer questions

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ const {
   insertRoleData,
   updateEmployeeRole,
 } = require("./utils/queries");
+
+const startingQuestions = [
+  {
+    type: "list",
+    name: "answer",
+    message: "What would you like to do?",
+    choices: [
+      "View all employees",
+      "Add an employee",
+      "Update an employee role",
+      "View all roles",
+      "Add a role",
+      "View all departments",
+      "Add a department",
+      "Quit",
+    ],
+  },
+];
+
+const employeeQuestions = [
+  {
+    type: "text",
+    name: "name",
+    message: "Add employee name:",
+  },
+];
