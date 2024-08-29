c1997936c43ec11a2c70608ece7b4d7cfb3b6a7a
rph <rhansen0127@gmail.com>
2024-08-29 15:40:23 -0500

    it is somehow working today

--- db/seed.sql
+++ db/seed.sql
@@ -20,5 +20,5 @@ INSERT INTO employees (first_name, last_name, role_id)
 VALUES
 ('John', 'Doe', 2),
 ('Jane', 'Smith', 1),
-('Sage', 'Hilbert', 7),
-('Basil', 'Hilbert', 2);
+('Ryan', 'Hansen', 7),
+('Test', 'Test', 2);
